60e45f5d0e5127fb6d0d1bff42e41c5768d45c2f
Yoberty Garcia <yoberty.garcia@appnovation.com>
2021-05-27 18:50:32 -0400

    003-products-list - Because we need to show a product list view

---
+++ resources/lang/es/products.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lineas de lenguaje para los Productos
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes lineas son usadas para las traducciones de las lineas
+    | de los productos
+    |
+    */
+
+    'headline' => 'Productos',
+    'id' => 'Id',
+    'name' => 'Nombre',
+    'description' => 'Descripción',
+    'price' => 'Precio',
+    'image' => 'Imagen',
+    'additional_info' => 'Información adicional',
+    'category' => 'Categoría',
+    'subcategory' => 'subcategoría',
+    'featured' => 'Destacado',
+    'actions' => 'Acciones',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('product.list');
+});
 
 require __DIR__.'/auth.php';
